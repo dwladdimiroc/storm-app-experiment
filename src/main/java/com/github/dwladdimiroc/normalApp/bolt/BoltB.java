19ead9b22e703adffd1bf47d27061d5926f54866
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2021-07-02 00:07:10 +0200

    Fix Executed Time

--- src/main/java/com/github/dwladdimiroc/normalApp/bolt/BoltB.java
+++ src/main/java/com/github/dwladdimiroc/normalApp/bolt/BoltB.java
@@ -41,7 +41,7 @@ public class BoltB implements IRichBolt, Serializable {
         this.outputCollector = collector;
         this.id = context.getThisComponentId();
 
-        this.array = new int[100000];
+        this.array = new int[40000];
         for (int i = 0; i < this.array.length; i++) {
             this.array[i] = i;
         }
