07d493286404eee7562cc2b012e6f34701976ba7
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2021-07-05 14:39:53 +0200

    Change ExecutedTime App

--- src/main/java/com/github/dwladdimiroc/normalApp/bolt/BoltF.java
+++ src/main/java/com/github/dwladdimiroc/normalApp/bolt/BoltF.java
@@ -38,7 +38,7 @@ public class BoltF implements IRichBolt, Serializable {
         this.outputCollector = collector;
         this.id = context.getThisComponentId();
 
-        this.array = new int[7000];
+        this.array = new int[14000];
         for (int i = 0; i < this.array.length; i++) {
             this.array[i] = i;
         }
