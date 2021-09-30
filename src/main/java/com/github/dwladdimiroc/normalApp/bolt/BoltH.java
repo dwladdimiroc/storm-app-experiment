8d75c10a6bb23767bcf488b3ec605ca82e1bfbe4
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2021-09-30 13:03:57 +0200

    Update complexity operators

--- src/main/java/com/github/dwladdimiroc/normalApp/bolt/BoltH.java
+++ src/main/java/com/github/dwladdimiroc/normalApp/bolt/BoltH.java
@@ -38,7 +38,7 @@ public class BoltH implements IRichBolt, Serializable {
         this.outputCollector = collector;
         this.id = context.getThisComponentId();
 
-        this.array = new int[15000];
+        this.array = new int[80000];
         for (int i = 0; i < this.array.length; i++) {
             this.array[i] = i;
         }
