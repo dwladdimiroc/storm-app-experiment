7d00bcaf5ae4bd5707f2f8270dabc347c1b17807
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2021-10-06 22:59:44 +0200

    Fix stream and executed time

--- src/main/java/com/github/dwladdimiroc/normalApp/topology/Topology.java
+++ src/main/java/com/github/dwladdimiroc/normalApp/topology/Topology.java
@@ -5,6 +5,7 @@ import com.github.dwladdimiroc.normalApp.spout.Spout;
 import org.apache.storm.Config;
 import org.apache.storm.StormSubmitter;
 import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
 
 import java.io.Serializable;
 
@@ -13,7 +14,7 @@ public class Topology implements Serializable {
 
     public static void main(String[] args) {
         Config config = new Config();
-        config.setMessageTimeoutSecs(125);
+        config.setMessageTimeoutSecs(128);
         config.setNumWorkers(3);
 
         TopologyBuilder builder = new TopologyBuilder();
@@ -24,30 +25,30 @@ public class Topology implements Serializable {
         // Set Bolt
         // Spout Twitter Streaming -> BoltA ParseData -> BoltB SpamDetector
         builder.setBolt("BoltA", new BoltA("BoltB"), 5).setNumTasks(5).
-                fieldsGrouping("Spout", "BoltA", "id-replica");
+                fieldsGrouping("Spout", "BoltA", new Fields("id-replica"));
         // BoltA ParseData -> BoltB SpamDetector -> BoltC UserDetect || BoltF NewsDetector
-        builder.setBolt("BoltB", new BoltB("BoltC", "BoltF"), 10).setNumTasks(10).
-                fieldsGrouping("BoltA", "BoltB", "id-replica");
+        builder.setBolt("BoltB", new BoltB("BoltC", "BoltF"), 20).setNumTasks(20).
+                fieldsGrouping("BoltA", "BoltB", new Fields("id-replica"));
         // BoltB SpamDetector -> BoltC UserDetect -> BoltD SendNotification
         builder.setBolt("BoltC", new BoltC("BoltD"), 5).setNumTasks(5).
-                fieldsGrouping("BoltB", "BoltC", "id-replica");
+                fieldsGrouping("BoltB", "BoltC", new Fields("data-1"));
         // BoltC UserDetect -> BoltD SendNotification -> BoltE DataSaved
         builder.setBolt("BoltD", new BoltD("BoltE"), 5).setNumTasks(5)
-                .fieldsGrouping("BoltC", "BoltD", "id-replica");
+                .fieldsGrouping("BoltC", "BoltD", new Fields("id-replica"));
         // BoltD SendNotification || BoltG SentimentalClassified -> BoltE DataSaved -> ACK
         builder.setBolt("BoltE", new BoltE(), 10).setNumTasks(10)
-                .fieldsGrouping("BoltD", "BoltE")
-                .fieldsGrouping("BoltG", "BoltE");
+                .fieldsGrouping("BoltD", "BoltE", new Fields("data-1"))
+                .fieldsGrouping("BoltG", "BoltE", new Fields("stream-2"));
         // BoltB SpamDetector -> BoltF NewsDetector -> BoltG TopicClassified || BoltH SentimentalClassified
         builder.setBolt("BoltF", new BoltF("BoltG", "BoltH"), 10).setNumTasks(10)
-                .fieldsGrouping("BoltB", "BoltF", "id-replica");
+                .fieldsGrouping("BoltB", "BoltF", new Fields("stream-2"));
         // BoltF NewsDetector || BoltH Sentimental Classified -> BoltG TopicClassified -> BoltE DataSaved
         builder.setBolt("BoltG", new BoltG("BoltE"), 10).setNumTasks(10)
-                .fieldsGrouping("BoltF", "BoltG")
-                .fieldsGrouping("BoltH", "BoltG");
+                .fieldsGrouping("BoltF", "BoltG", new Fields("data-1"))
+                .fieldsGrouping("BoltH", "BoltG", new Fields("id-replica"));
         // BoltF NewsDetector -> BoltH Sentimental Classified -> BoltG TopicClassified
         builder.setBolt("BoltH", new BoltH("BoltG"), 20).setNumTasks(20)
-                .fieldsGrouping("BoltF", "BoltH", "id-replica");
+                .fieldsGrouping("BoltF", "BoltH", new Fields("stream-2"));
 
         try {
             StormSubmitter.submitTopology(TOPOLOGY_NAME, config, builder.createTopology());
