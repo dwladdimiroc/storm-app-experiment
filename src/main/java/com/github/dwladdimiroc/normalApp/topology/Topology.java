6aa9dd38247ccf891872feb139a8fa5592525985
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2022-02-02 11:55:32 +0100

    Fix Topology

--- src/main/java/com/github/dwladdimiroc/normalApp/topology/Topology.java
+++ src/main/java/com/github/dwladdimiroc/normalApp/topology/Topology.java
@@ -25,11 +25,19 @@ public class Topology implements Serializable {
 
         // Set Spout
         builder.setSpout("Spout", new Spout(args[0], "BoltA"), 1);
+
         // Set Bolt
-        builder.setBolt("BoltA", new BoltA("BoltB"), 25).setNumTasks(25).fieldsGrouping("Spout", "BoltA", new Fields("id-replica"));
-        builder.setBolt("BoltB", new BoltB("BoltC"), 25).setNumTasks(25).fieldsGrouping("BoltA", "BoltB", new Fields("id-replica"));
-        builder.setBolt("BoltC", new BoltC("BoltD"), 25).setNumTasks(25).fieldsGrouping("BoltB", "BoltC", new Fields("id-replica"));
-        builder.setBolt("BoltD", new BoltD(), 25).setNumTasks(25).fieldsGrouping("BoltC", "BoltD", new Fields("id-replica"));
+        builder.setBolt("BoltA", new BoltA("BoltB"), 25).setNumTasks(25)
+                .fieldsGrouping("Spout", "BoltA", new Fields("id-replica"));
+
+        builder.setBolt("BoltB", new BoltB("BoltC"), 25).setNumTasks(25)
+                .fieldsGrouping("BoltA", "BoltB", new Fields("id-replica"));
+
+        builder.setBolt("BoltC", new BoltC("BoltD"), 25).setNumTasks(25)
+                .fieldsGrouping("BoltB", "BoltC", new Fields("id-replica"));
+
+        builder.setBolt("BoltD", new BoltD(), 25).setNumTasks(25)
+                .fieldsGrouping("BoltC", "BoltD", new Fields("id-replica"));
 
         try {
             StormSubmitter.submitTopology(TOPOLOGY_NAME, config, builder.createTopology());
