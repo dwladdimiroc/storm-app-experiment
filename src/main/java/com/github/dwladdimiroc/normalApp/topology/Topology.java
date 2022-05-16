34b17991686fc3caaffeef727ee9a52ecb24e398
Daniel Wladdimiro <daniel.wladdimiro@lip6.fr>
2022-05-16 10:59:59 +0200

    Add args

--- src/main/java/com/github/dwladdimiroc/normalApp/topology/Topology.java
+++ src/main/java/com/github/dwladdimiroc/normalApp/topology/Topology.java
@@ -21,18 +21,20 @@ public class Topology implements Serializable {
         config.setMessageTimeoutSecs(125);
         config.setNumWorkers(7);
 
+        int numParallelism = Integer.parseInt(args[1]);
+
         TopologyBuilder builder = new TopologyBuilder();
 
         // Set Spout
         builder.setSpout("Spout", new Spout(args[0], "BoltA"), 1);
         // Set Bolt
-        builder.setBolt("BoltA", new BoltA("BoltB"), 10).setNumTasks(10).
+        builder.setBolt("BoltA", new BoltA("BoltB"), numParallelism).setNumTasks(numParallelism).
                 shuffleGrouping("Spout", "BoltA");
-        builder.setBolt("BoltB", new BoltB("BoltC"), 20).setNumTasks(20).
+        builder.setBolt("BoltB", new BoltB("BoltC"), numParallelism).setNumTasks(numParallelism).
                 shuffleGrouping("BoltA", "BoltB");
-        builder.setBolt("BoltC", new BoltC("BoltD"), 15).setNumTasks(15).
+        builder.setBolt("BoltC", new BoltC("BoltD"), numParallelism).setNumTasks(numParallelism).
                 shuffleGrouping("BoltB", "BoltC");
-        builder.setBolt("BoltD", new BoltD(), 10).setNumTasks(10).
+        builder.setBolt("BoltD", new BoltD(), numParallelism).setNumTasks(numParallelism).
                 shuffleGrouping("BoltC", "BoltD");
 
         try {
